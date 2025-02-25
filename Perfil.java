f0f855ef2053e95098c5d661dff541d5ce3ce6ed
Mariana <marianalmmj@gmail.com>
2025-02-24 21:09:46 -0300

    Add files via upload

---
+++ Perfil.java
@@ -0,0 +1,10 @@
+
+public class Perfil{
+
+    public static void main(String[] args) {
+        System.out.println("Nome:");
+        System.out.println("Idade:");
+        System.out.println("Interesse:");
+
+    }
+}
